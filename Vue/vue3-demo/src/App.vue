c4a1c893da2f914b14610231e4151eb74c87066e
smallsky2516 <smallsky2516@yeah.net>
2024-02-20 22:38:19 +0800

    feat: init commit vue3-demo

---
+++ Vue/vue3-demo/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="main">
+    <button type="button" @click="increment">Count：{{ count }}</button>
+    <div>{{ () => 1 + 2 }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const count = ref(0)
+
+const increment = () => count.value++
+
+onMounted(() => {
+  console.log('component mounted')
+})
+</script>
